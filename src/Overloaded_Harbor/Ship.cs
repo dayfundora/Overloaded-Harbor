7c271c5a3375aedb4e05f3f21117bf617b6098d8
dayfundora <dayfundoraglez@gmail.com>
2021-01-23 00:35:50 -0500

    Add ShipType

---
+++ src/Overloaded_Harbor/Ship.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Harbor
+{
+    public enum ShipType { Small, Medium, Large };
+
+}
