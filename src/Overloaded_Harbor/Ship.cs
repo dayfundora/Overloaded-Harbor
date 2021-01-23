69db95cdd224f03203bdfa91e5219ad2bc6d069f
dayfundora <dayfundoraglez@gmail.com>
2021-01-23 00:36:30 -0500

    Create Ship Class

--- src/Overloaded_Harbor/Ship.cs
+++ src/Overloaded_Harbor/Ship.cs
@@ -8,4 +8,27 @@ namespace Overloaded_Harbor
 {
     public enum ShipType { Small, Medium, Large };
 
+    public class Ship
+    {
+        public Ship(ShipType type, double arrivalToHarbor)
+        {
+            Type = type;
+            ArraivalToHarbor = arrivalToHarbor;
+        }
+        public ShipType Type { get; set; }
+        public double ArraivalToHarbor { get; set; }
+        public double EntranceToHarbor { get; set; }
+        public double ArrivalToDock { get; set; }
+        public double LoadEnd { get; set; }
+        public double ExitFromDock { get; set; }
+        public double HarborExit { get; set; }
+        public double WaitTime
+        {
+            get { return (EntranceToHarbor - ArraivalToHarbor + ExitFromDock - LoadEnd) / 60 / 60; }
+        }
+        public double LoadTime
+        {
+            get { return (LoadEnd - ArrivalToDock) / 60 / 60; }
+        }
+    }
 }
