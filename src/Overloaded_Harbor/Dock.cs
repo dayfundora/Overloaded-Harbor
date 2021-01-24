b3125e313717169940d6280e8f785c3aae03a8ed
dayfundora <dayfundoraglez@gmail.com>
2021-01-24 00:37:31 -0500

    Create Dock.cs

---
+++ src/Overloaded_Harbor/Dock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Harbor
+{
+    public class Dock
+    {
+        public double LoadEnd { get; set; }
+        public Ship Ship { get; set; }
+    }
+
+}
