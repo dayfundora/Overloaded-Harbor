f4b72fadc56ba90b80f62cb52cd68a71fa4dd159
dayfundora <dayfundoraglez@gmail.com>
2021-01-25 00:40:52 -0500

    Create Harbor.cs

---
+++ src/Overloaded_Harbor/Harbor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Harbor
+{
+    public class Harbor
+    {
+
+        public Harbor(int docks = 4)
+        {
+            DocksCount = docks;
+        }
+
+        public int DocksCount { get; set; }
+
+    }
+}
