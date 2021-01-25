5389389f3a23ec4dab0fdbd09c3f4b8f27a115f9
dayfundora <dayfundoraglez@gmail.com>
2021-01-25 01:46:33 -0500

    Harbor Simulation

--- src/Overloaded_Harbor/Harbor.cs
+++ src/Overloaded_Harbor/Harbor.cs
@@ -8,6 +8,13 @@ namespace Overloaded_Harbor
 {
     public class Harbor
     {
+        Queue<Ship> shipsInHold;
+        Queue<Ship> shipsAttended;
+        Dock[] docks;
+        bool tugboatInDock;//true dock   false harbor
+        double time;
+        double arrival;
+        Random random;
 
         public Harbor(int docks = 4)
         {
@@ -16,5 +23,23 @@ namespace Overloaded_Harbor
 
         public int DocksCount { get; set; }
 
+        private void InitializeSistem()
+        {
+            random = new Random();
+            shipsInHold = new Queue<Ship>();
+            shipsAttended = new Queue<Ship>();
+            tugboatInDock = false;
+            time = arrival = GenerateArrivalHarbor();
+
+            docks = new Dock[DocksCount];
+            for (int i = 0; i < DocksCount; i++)
+                docks[i] = new Dock();
+        }
+
+        private double GenerateArrivalHarbor()
+        {
+            double d = Distribution.Exponential(random, 0.125);
+            return d * 60 * 60;
+        }
     }
 }
