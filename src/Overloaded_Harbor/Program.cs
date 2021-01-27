23aa90646c62c8f9307c7ccd11285381fddeaefc
dayfundora <dayfundoraglez@gmail.com>
2021-01-27 02:56:19 -0500

    Example

--- src/Overloaded_Harbor/Program.cs
+++ src/Overloaded_Harbor/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace Overloaded_Harbor
 {
@@ -6,7 +12,17 @@ namespace Overloaded_Harbor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+                Tuple<double, double, Queue<Ship>> datos;
+                int hours = 480;
+                Harbor harbor = new Harbor();
+                datos = harbor.StartSimulation(hours);
+
+                Console.WriteLine("Average Wait in Harbor: " + datos.Item1);
+                Console.WriteLine("Average Wait in Dock: " + datos.Item2);
+
+                foreach (var item in datos.Item3)
+                    Console.WriteLine(item.Type + "    " + item.WaitTime + "    " + item.LoadTime);
         }
     }
 }
